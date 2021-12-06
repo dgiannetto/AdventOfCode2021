6b6a223e9741cf488bc5d4b4f79eebfd1c024d9a
David Giannetto <david.w.giannetto@gmail.com>
2021-12-06 09:32:18 -0600

    day 6

---
+++ Day 6/2.php
@@ -0,0 +1,45 @@
+<?php
+
+$file = file('input.txt', FILE_IGNORE_NEW_LINES);
+
+$fish = explode(',', $file[0]);
+
+$counts = array_reduce($fish, function ($counts, $item) {
+    if (is_null($counts)) {
+        $counts = [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ];
+    }
+
+    $counts[(int)$item]++;
+    return $counts;
+});
+
+for ($i = 0; $i < 256; $i++) {
+    processCounts($counts);
+}
+
+echo array_sum($counts);
+
+
+function processCounts(array &$counts): void
+{
+    $zeroes = null;
+    foreach ($counts as $count => $value) {
+        if ($count === 0) {
+            $zeroes = $value;
+        } else {
+            $counts[$count-1] = $value;
+        }
+    }
+    $counts[6] += $zeroes;
+    $counts[8] = $zeroes;
+}
